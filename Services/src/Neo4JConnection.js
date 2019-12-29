ccb25baf44f8ba8bf86277afb5139c99888cabb1
Stefan <vasovmail@gmail.com>
2019-12-29 21:09:55 +0100

    Funkcionalnosti za neo4j

---
+++ Services/src/Neo4JConnection.js
@@ -0,0 +1,8 @@
+var neo4j = require('neo4j-driver');
+var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','nbpnbp'));
+var session = driver.session();
+
+module.exports =
+ {
+     session,driver
+ };
