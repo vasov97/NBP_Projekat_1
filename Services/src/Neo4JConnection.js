ad317b4a0ad1268bba84cb4dccc661ed126763bd
Uros <uros.aleksandrovic@outlook.com>
2020-01-08 01:10:24 +0100

    Testiranje funkcija za usere i postove, sredjivanje manjih bagova. Uvodjenje kesa kod komentara, pocetak.

--- Services/src/Neo4JConnection.js
+++ Services/src/Neo4JConnection.js
@@ -1,5 +1,27 @@
 var neo4j = require('neo4j-driver');
-var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','nbpnbp'));
+var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','nbp'));
 var session = driver.session();
 
-module.exports = {driver:driver,session:session};
+
+function createError(message){
+    let responseMessage = message
+    if (responseMessage === undefined)
+        responseMessage = "ERROR";
+    return error =  {
+        message : responseMessage,
+        status : 400
+    };
+}
+
+function createResponse(message, optionalObject){
+    let responseMessage = message
+    if (responseMessage === undefined)
+        responseMessage = "OK";
+    return response =  {
+        message : responseMessage,
+        object : optionalObject,
+        status : 200
+      }
+}
+
+module.exports = {driver:driver,session:session,createError,createResponse};
